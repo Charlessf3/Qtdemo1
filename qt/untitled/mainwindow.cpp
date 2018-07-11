3b378d483d0cc2c62a6286582429e33b08e4e935
Charlessf3 <2623955134@qq.com>
2018-07-11 13:22:00 +0800

    第一次测试

---
+++ qt/untitled/mainwindow.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "dialog.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->pushButton->setText(tr("下一页"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QDialog *dlg = new QDialog(this);
+    dlg->show();
+}
